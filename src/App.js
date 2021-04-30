fd2154f219a08f78e74f0e6bb2b8eae872430d6b
Rupinder <rupindersinghsaini1999@gmail.com>
2021-04-30 19:39:49 +0530

    project added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Header from './components/Header'
+import NewHome from './components/HomeTable'
+import Upper from './components/Upper'
+import Grid from '@material-ui/core/Grid';
+import HomeSide from './components/HomeSide'
+const App = () => {
+    return (<>
+   <Grid item sm={12}>
+        <Header />
+        </Grid>
+   
+   <Grid container  direction='column' spacing='4' alignItems='center'>
+        
+        <Upper />
+        <Grid  container item xs={11} spacing='4' style={{marginTop:'6rem'}} >
+            <NewHome />
+            <HomeSide />
+        </Grid> 
+    </Grid> 
+    </>
+    )
 }
 
-export default App;
+export default App
